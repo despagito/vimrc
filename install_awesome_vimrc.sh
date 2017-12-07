20a3c0ae48af1c310fccaf519900a39933574ba2
John Young <9820592+despagito@users.noreply.github.com>
2017-12-07 11:06:54 +0800

    Update install_awesome_vimrc.sh
    add set line number to .vimrc

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -10,6 +10,8 @@ source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
 
+set nu
+
 try
 source ~/.vim_runtime/my_configs.vim
 catch
